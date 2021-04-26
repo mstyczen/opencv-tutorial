02a76fb7018cebeeedf35a6ef478d5c117c7cd0c
mstyczen <42466994+mstyczen@users.noreply.github.com>
2021-04-26 14:57:58 +0200

    chapter 7

---
+++ chapter7.py
@@ -0,0 +1,74 @@
+import numpy as np
+import cv2
+
+path = 'resources/lambo.PNG'
+
+
+def empty(_):
+    pass
+
+
+def stackImages(scale, imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range(0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]),
+                                                None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank] * rows
+        hor_con = [imageBlank] * rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor = np.hstack(imgArray)
+        ver = hor
+    return ver
+
+
+# use track bars for hue, sat and val
+cv2.namedWindow("TrackBars")
+cv2.resizeWindow("TrackBars", 640, 240)
+# 179 is max value for hue in OpenCV
+cv2.createTrackbar("Hue Min", "TrackBars", 0, 179, empty)
+cv2.createTrackbar("Hue Max", "TrackBars", 19, 179, empty)
+cv2.createTrackbar("Sat Min", "TrackBars", 110, 255, empty)
+cv2.createTrackbar("Sat Max", "TrackBars", 240, 255, empty)
+cv2.createTrackbar("Val Min", "TrackBars", 153, 255, empty)
+cv2.createTrackbar("Val Max", "TrackBars", 255, 255, empty)
+
+while True:
+    img = cv2.imread(path)
+    # convert to HSV space
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    hue_min = cv2.getTrackbarPos("Hue Min", "TrackBars")
+    hue_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+    sat_min = cv2.getTrackbarPos("Sat Min", "TrackBars")
+    sat_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
+    val_min = cv2.getTrackbarPos("Val Min", "TrackBars")
+    val_max = cv2.getTrackbarPos("Val Max", "TrackBars")
+
+    lower = np.array([hue_min, sat_min, val_min])
+    upper = np.array([hue_max, sat_max, val_max])
+    mask = cv2.inRange(imgHSV, lower, upper)
+    imgResult = cv2.bitwise_and(img, img, mask=mask)
+
+    img_stack = stackImages(0.6, [[img, imgHSV], [mask, imgResult]])
+    cv2.imshow("stack", img_stack)
+    cv2.waitKey(1)
